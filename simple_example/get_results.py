fd2c44cb6fc8ea23aba833e4551fc68b21154fd7
buxizhizhoum <mapeaks@126.com>
2018-05-30 18:32:21 +0800

    add while loop

--- simple_example/get_results.py
+++ simple_example/get_results.py
@@ -6,9 +6,12 @@ A simple example to get result from celery backend
 import time
 from tasks import sendmail
 from tasks import add
-# result = sendmail.delay("Einstein")
-result = add.delay(1, 2)
 
-while not result.ready():
-    time.sleep(1)
-print("task done: %s" % result.get())
+if __name__ == "__main__":
+    while True:
+        # result = sendmail.delay("Einstein")
+        result = add.delay(1, 2)
+
+        while not result.ready():
+            time.sleep(1)
+        print("task done: %s" % result.get())
