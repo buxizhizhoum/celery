9c23a6402051cb98daded21750d5eb6f7c0a88d9
buxizhizhoum <mapeaks@126.com>
2018-05-30 18:27:34 +0800

    add queue

--- producer_consumer/celery_config.py
+++ producer_consumer/celery_config.py
@@ -6,6 +6,10 @@ from celery.schedules import crontab
 BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_IMPORTS = ['producer_consumer']
+CELERY_DEFAULT_QUEUE = 'pc'  # separate different celery instance
+# CELERY_ROUTES = {
+#     'producer_consumer': {'queue': 'pc'},
+# }
 # CELERYBEAT_SCHEDULE = {
 #         "every-1-minute": {
 #             # 'task': 'tasks.sendmail',
